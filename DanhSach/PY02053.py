3d56d2147bf4b4f28aa047095f020e3fd1e1b82f
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2022-08-05 22:03:16 +0700

    Add files via upload

---
+++ DanhSach/PY02053.py
@@ -0,0 +1,20 @@
+def run():
+    n = int(input())
+    tren = 0
+    duoi = 0
+    for i in range(n):
+        x = [int(i) for i in input().split()]
+        for j in range(0, n - i - 1):
+            tren += x[j]
+        for j in range(n - i, n):
+            duoi += x[j]
+    tmp = abs(tren - duoi)
+    k = int(input())
+    if(tmp <= k):
+        print("YES")
+    else:
+        print("NO")
+    print(tmp)
+
+
+run()
