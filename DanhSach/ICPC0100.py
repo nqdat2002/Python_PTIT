7ebc61c415b91cc042a80b9699b0ab51390f2c37
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2023-02-21 08:05:28 +0700

    Update ICPC0100.py

--- DanhSach/ICPC0100.py
+++ DanhSach/ICPC0100.py
@@ -1,13 +1,11 @@
-t = int(input())
-while t > 0 :
+for t in range(int(input())):
     n = int(input())
     a = [int(i) for i in input().split()]
     cnt = 0
     for i in range(0, n - 1):
         x = max(a[i], a[i + 1])
         y = min(a[i], a[i + 1])
-        while x < (y << 1):
+        while x > (y << 1):
             cnt = cnt + 1
             y <<= 1
     print(cnt)
-    t = t - 1
